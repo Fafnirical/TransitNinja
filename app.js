e7f564f65f4268c997d0cea3e0721da41fe1b799
Fafnirical <wade.burelbach@gmail.com>
2014-11-27 23:12:02 -0500

    Whitespace
    Spaces -> tabs

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 /*** BEGIN Requirements ***/
-var //
-    express      = require('express'),
-    path         = require('path'),
-    logger       = require('morgan'),
-    stylus       = require('stylus');
+var express = require('express'),
+    path    = require('path'),
+    logger  = require('morgan'),
+    stylus  = require('stylus');
 
 var app = express();
 /*** END Requirements ***/
@@ -18,16 +17,16 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());*/
 app.use(stylus.middleware({
-  src: path.join(__dirname, 'src'),
-  dest: path.join(__dirname, 'public'),
-  compile: function(str, path) {
-    return stylus(str)
-      .set('filename', path)
-      .set('compress', true);
-  }
+	src: path.join(__dirname, 'src'),
+	dest: path.join(__dirname, 'public'),
+	compile: function(str, path) {
+		return stylus(str)
+		.set('filename', path)
+		.set('compress', true);
+	}
 }));
 app.use(express.static(
-  path.join(__dirname, 'public')
+	path.join(__dirname, 'public')
 ));
 /*** END Configuration ***/
 
