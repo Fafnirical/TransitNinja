b38d97c74b45bafce0c522aafa71e53c679c5849
Fafnirical <wade.burelbach@gmail.com>
2015-01-11 15:19:33 -0500

    Merge remote-tracking branch 'origin/master' into postgresql

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gtfs',
-	agencies: [
-		{
-			agency_key: 'capital-metro',
-			url: 'download/capital-metro_20141209_0128.zip'
-		}
-	]
-};
